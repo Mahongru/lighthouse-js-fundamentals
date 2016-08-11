6e7bc6ffeab1b26cf66333a3b2bd81cbde7933de
mahongru <mahongru.ca@gmail.com>
2016-08-11 22:22:49 +0000

    X

--- age-calculator.js
+++ age-calculator.js
@@ -1,6 +1,15 @@
-var city = "Vancouver";
-var yearIncorporated = 1886;
-var currentYear = 2015;
-var age = currentYear - yearIncorporated;
+function ageCalculator (name, birthday, currentYear) {
+  age = currentYear - birthday;
+  return name + " is " + age + " years old.";
+};
 
-city + " is " + age + " years old.";
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+/*
+var name = "Suzie";
+var birthday = 1983;
+var currentYear = 2015;
+var age = currentYear - birthday;
+name + " is " + age + " years old.";
+*/
